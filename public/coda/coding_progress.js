21d5e04c2c9f32eeea23fcb1a3b9c93da410a1a1
Isaack Mwenda <mwendaisaac@gmail.com>
2018-11-30 14:44:03 +0300

    Added dataset entries links to coda and table sorting on data update
    Added dataset entries links to coda and table sorting on data update

--- public/coda/coding_progress.js
+++ public/coda/coding_progress.js
@@ -12,11 +12,17 @@ function update_progress_ui(data) {
     }
     last_update = data["last_update"]
     document.getElementById('last_update').innerText = "Last updated: " + last_update
+    Ke_time=new Date(last_update)
+    console.log(Ke_time);
     for (var dataset_id in data["coding_progress"]) {
         var messages_count = data["coding_progress"][dataset_id]["messages_count"]
         var messages_with_label = data["coding_progress"][dataset_id]["messages_with_label"]
+        var dataset_url = document.createElement("a")
+            dataset_url.setAttribute("href", "https://web-coda.firebaseapp.com/?dataset="+dataset_id)
+            dataset_url.setAttribute('target', '_blank')
+            dataset_url.innerText=dataset_id     
         rw = status_body.insertRow()
-        rw.insertCell().innerText = dataset_id
+        rw.insertCell().appendChild(dataset_url)
         rw.insertCell().innerText = messages_count
         rw.insertCell().innerText = messages_with_label
         rw.insertCell().innerText = (100 * messages_with_label / messages_count).toFixed(2) + '%'
@@ -27,8 +33,11 @@ function update_progress_ui(data) {
             $('#codingtable td:nth-child(4)').each(function() {
                var Done = $(this).text();
                 //Style the entire row conditionally based on the cell value
-                if ((parseFloat(Done) >= 0) && (parseFloat(Done) <= 25)) {
-                    $(this).parent().addClass('coding-below25'); 
+                if ((parseFloat(Done) === 0)) {
+                    $(this).parent().addClass('coding-notstarted'); 
+                }
+                else if((parseFloat(Done) > 0) && (parseFloat(Done) <= 25)) {
+                        $(this).parent().addClass('coding-below25'); 
                 }
                 else if((parseFloat(Done) > 25) && (parseFloat(Done) <= 50)) {
                     $(this).parent().addClass('coding-above25'); 
@@ -48,8 +57,10 @@ function update_progress_ui(data) {
         $(function() {
             $("#codingtable").tablesorter({
                 //sorting on page load, column four in descending order i.e from least coded to most coded.
-                    sortList : [[3,0]]
+                sortList : [[3,0]]                      
             });
+            //Trigger sorting on table data update
+            $('#codingtable').tablesorter().trigger('update');
         });   
     }
 }
