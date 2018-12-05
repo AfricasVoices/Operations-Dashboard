b7c0236f3952ec0cba14f4d3610dc933857ab7c6
Isaack Mwenda <mwendaisaac@gmail.com>
2018-12-05 15:37:40 +0300

    Added google firebase authentication
    Created a Login page
    Added google firebase authentication

--- public/coda/coding_progress.js
+++ public/coda/coding_progress.js
@@ -11,14 +11,14 @@ function update_progress_ui(data) {
         status_body.removeChild(status_body.firstChild);
     }
     last_update = data["last_update"]
-    document.getElementById('last_update').innerText = "Last updated: " + last_update
+    document.getElementById('last_update').innerText = "Last updated: " + last_update   
     for (var dataset_id in data["coding_progress"]) {
         var messages_count = data["coding_progress"][dataset_id]["messages_count"]
         var messages_with_label = data["coding_progress"][dataset_id]["messages_with_label"]
         var dataset_link = document.createElement("a")
             dataset_link.setAttribute("href", "https://web-coda.firebaseapp.com/?dataset="+dataset_id)
             dataset_link.setAttribute('target', '_blank')
-            dataset_link.innerText=dataset_id     
+            dataset_link.innerText=dataset_id          
         rw = status_body.insertRow()
         rw.insertCell().appendChild(dataset_link)
         rw.insertCell().innerText = messages_count
@@ -26,7 +26,7 @@ function update_progress_ui(data) {
         rw.insertCell().innerText = (100 * messages_with_label / messages_count).toFixed(2) + '%'
         console.log(dataset_id, messages_count, messages_with_label);
         //Formating rows based on cell value
-        $(document).ready(function(){
+        $(document).ready(function(){ 
             //Grab the cells of the last rows
             $('#codingtable td:nth-child(4)').each(function() {
                var Done = $(this).text();
@@ -49,9 +49,9 @@ function update_progress_ui(data) {
                 else {
                     $(this).parent().addClass('coding-complete'); 
                 }
-            });
+            }); 
         });
-        //Table sorting using tablesorter plugin based on fraction of message labelling complete
+        // Table sorting using tablesorter plugin based on fraction of message labelling complete    
         $(function() {
             $("#codingtable").tablesorter({
                 //sorting on page load, column four in descending order i.e from least coded to most coded.
@@ -59,6 +59,7 @@ function update_progress_ui(data) {
             });
             //Trigger sorting on table data update
             $('#codingtable').tablesorter().trigger('update');
-        });   
+        }); 
     }
 }
+
