89ba4f575979a7c3e2df86beccd2e790e6e510f3
Alexander Simpson <as2388@cam.ac.uk>
2019-05-03 16:54:08 +0300

    Implement MAX_BATCH_SIZE

--- sms_statistics_service/src/firestore_client.py
+++ sms_statistics_service/src/firestore_client.py
@@ -8,6 +8,8 @@ log = Logger(__name__)
 
 
 class FirestoreClient(object):
+    MAX_BATCH_SIZE = 500
+
     def __init__(self, cert):
         """
         :param cert: Path to a certificate file or a dict representing the contents of a certificate.
@@ -25,9 +27,9 @@ class FirestoreClient(object):
 
     def get_active_projects(self):
         """
-        Downloads all the active projects from Firestore
+        Downloads all the active projects from Firestore.
 
-        :return: list of active projects
+        :return: list of active projects.
         :rtype: list of ActiveProject
         """
         active_projects = []
@@ -37,13 +39,14 @@ class FirestoreClient(object):
 
     def update_sms_stats(self, project_name, iso_string, sms_stats):
         """
+        Updates the SMS stats for the given project and timestamp.
 
-        :param project_name:
+        :param project_name: Name of project to update the SMS stats of.
         :type project_name: str
-        :param iso_string:
+        :param iso_string: ISO 8601 formatted string to update the SMS stats of.
         :type iso_string: str
-        :param sms_stats:
-        :type sms_stats: data_models.SMSStats
+        :param sms_stats: SMS stats to update with.
+        :type sms_stats: src.data_models.SMSStats
         """
         log.info(f"Updating SMS stats for project {project_name} at time {iso_string}...")
         self._get_sms_stat_doc_ref(project_name, iso_string).set(sms_stats.to_dict())
@@ -51,37 +54,31 @@ class FirestoreClient(object):
 
     def update_sms_stats_batch(self, project_name, sms_stats_batch):
         """
+        Updates a batch of SMS stats for the given project.
 
-        :param project_name:
+        :param project_name: Name of project to update the SMS stats of.
         :type project_name: str
-        :param sms_stats_batch:
-        :type sms_stats_batch: dict of str -> data_models.SMSStats
+        :param sms_stats_batch: Dictionary of iso_string -> SMSStats to update that timestamp with.
+        :type sms_stats_batch: dict of str -> src.data_models.SMSStats
         """
         log.info(f"Batch updating {len(sms_stats_batch)} SMS stats for project {project_name}...")
         batch = self.client.batch()
+        batch_counter = 0
+        total_progress = 0
         for iso_string, sms_stats in sms_stats_batch.items():
             batch.set(self._get_sms_stat_doc_ref(project_name, iso_string), sms_stats.to_dict())
+            batch_counter += 1
+            total_progress += 1
+
+            if batch_counter >= self.MAX_BATCH_SIZE:
+                batch.commit()
+                log.info(f"Batch of {batch_counter} messages committed, progress: {total_progress} / {len(sms_stats_batch)}")
+                batch = self.client.batch()
+                batch_counter = 0
+
+        if batch_counter > 0:
+            batch.commit()
+            log.info(f"Final batch of {batch_counter} messages committed")
+
         batch.commit()
         log.info("SMS stats updated")
-
-        # total_messages_count = len(messages)
-        # i = 0
-        # batch_counter = 0
-        # batch = client.batch()
-        # for message in messages:
-        #     i += 1
-        #     id = message["MessageID"]
-        #     batch.set(get_message_ref(dataset_id, id), message)
-        #     batch_counter += 1
-        #     if batch_counter >= batch_size:
-        #         batch.commit()
-        #         print(
-        #             "Batch of {} messages committed, progress: {} / {}".format(batch_counter, i, total_messages_count))
-        #         batch_counter = 0
-        #         batch = client.batch()
-        #
-        # if batch_counter > 0:
-        #     batch.commit()
-        #     print("Final batch of {} messages committed".format(batch_counter))
-        #
-        # print("Written {} messages".format(i))
