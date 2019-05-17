19a7a92280d6c7921312d3bfb3f2d958827ea24b
Isaack Mwenda <mwendaisaac@gmail.com>
2019-05-17 11:01:13 +0300

    Remove test firestore configurations

--- public/media_engagement/firebase.js
+++ public/media_engagement/firebase.js
@@ -1,10 +1,11 @@
 var config = {
-    apiKey: "AIzaSyCiVcO6RuGxMPMDI2SiTpjcdXtko-PFAbw",
-    authDomain: "learning-d3.firebaseapp.com",
-    databaseURL: "https://learning-d3.firebaseio.com",
-    projectId: "learning-d3",
-    storageBucket: "learning-d3.appspot.com",
-    messagingSenderId: "405879847462"
+    apiKey: "AIzaSyCUHQE4gDk35-BtZrlNEkNbr1nX2Yn4l5Q",
+    authDomain: "avf-dashboards.firebaseapp.com",
+    databaseURL: "https://avf-dashboards.firebaseio.com",
+    projectId: "avf-dashboards",
+    storageBucket: "avf-dashboards.appspot.com",
+    messagingSenderId: "1063777163571",
+    appId: "1:1063777163571:web:57361756b9ecb959"
 };
 firebase.initializeApp(config);
 const mediadb = firebase.firestore();
