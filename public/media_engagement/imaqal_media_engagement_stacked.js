1a271eeacb3955378333ad634abcb85e4b570a5c
ritazagoni <zagoni.rita@gmail.com>
2019-07-02 18:13:36 +0300

    Stacked bar charts for received and sent sms

--- public/media_engagement/imaqal_media_engagement_stacked.js
+++ public/media_engagement/imaqal_media_engagement_stacked.js
@@ -75,17 +75,6 @@ const y_total_received_sms = d3.scaleLinear().range([Height, 0]);
 const y_total_sent_sms = d3.scaleLinear().range([Height, 0]);
 const y_total_failed_sms = d3.scaleLinear().range([Height, 0]);
 
-// Define line paths for total received sms(s)
-const total_received_line = d3.line()
-    .curve(d3.curveLinear)
-    .x(function (d) { return x(new Date(d.datetime)) })
-    .y(function (d) { return y_total_received_sms(d.total_received); })
-
-// Define line paths for total sent sms(s)
-const total_sent_line = d3.line()
-    .curve(d3.curveLinear)
-    .x(function (d) { return x(new Date(d.datetime)) })
-    .y(function (d) { return y_total_sent_sms(d.total_sent); })
 
 // Define line paths for total failed sms(s)
 const total_failed_line = d3.line()
@@ -93,10 +82,13 @@ const total_failed_line = d3.line()
     .x(function (d) { return x(new Date(d.datetime)) })
     .y(function (d) { return y_total_failed_sms(d.total_errored); })
 
+
+// Create line path element for failed line graph
+const total_failed_path = total_failed_sms_graph.append('path');
+
 // Function to update data
 const update = (data) => {
 
-    let operators = new Set()
     // format the data  
     data.forEach(function (d) {
         d.datetime = new Date(d.datetime);
@@ -104,39 +96,48 @@ const update = (data) => {
         d.total_sent = +d.total_sent
         d.total_pending = +d.total_pending
         d.total_errored = +d.total_errored
-        d.operators = new Object(d.operators)
-        d.NC_received = d.operators.NC.received
-        d.telegram_received = d.operators.telegram.received
-        const ordered_operators = {};
-        Object.keys(d.operators).sort().forEach(function(key) {
-            ordered_operators[key] = d.operators[key]
-            if (!(key in operators)) {
-                operators.add(key)
-            };
-        });
-        d.operators = ordered_operators
+        d.golis_received= +d.operators["golis"]["received"]
+        d.hormud_received= +d.operators["hormud"]["received"]
+        d.nationlink_received= +d.operators["nationlink"]["received"]
+        d.somnet_received= +d.operators["somnet"]["received"]
+        d.somtel_received= +d.operators["somtel"]["received"]
+        d.telesom_received= +d.operators["telesom"]["received"]
+        d.golis_sent= +d.operators["golis"]["sent"]
+        d.hormud_sent= +d.operators["hormud"]["sent"]
+        d.nationlink_sent= +d.operators["nationlink"]["sent"]
+        d.somnet_sent= +d.operators["somnet"]["sent"]
+        d.somtel_sent= +d.operators["somtel"]["sent"]
+        d.telesom_sent= +d.operators["telesom"]["sent"]
     });
 
     data.sort((a, b) => new Date(a.datetime) - new Date(b.datetime));
 
     data = data.filter(a => a.datetime > new Date("2019-07-01T12:30:00+00:00"));
     
-    keys = ["NC_received", "golis_received", "hormud_received", "nationlink_received", "somnet_received", 
-    "somtel_received", "telegram_received", "telesom"]
-    let stack = d3.stack()
-            .keys(keys)
-    let dataPointsStacked = stack(data)
-    console.log(dataPointsStacked)
+    receivedKeys = ["NC_received", "golis_received", "hormud_received", "nationlink_received", "somnet_received", 
+    "somtel_received", "telegram_received", "telesom_received"]
+
+    let stackReceived = d3.stack()
+            .keys(receivedKeys)
+    let receivedDataStacked = stackReceived(data)
+
+    sentKeys = ["NC_sent", "golis_sent", "hormud_sent", "nationlink_sent", "somnet_sent", 
+    "somtel_sent", "telegram_sent", "telesom_sent"]
+
+    let stackSent = d3.stack()
+            .keys(sentKeys)
+    let sentdDataStacked = stackSent(data)
+
 
     // set scale domains
     x.domain(d3.extent(data, d => new Date(d.datetime)));
-    y_total_received_sms.domain([0, d3.max(data, function (d) { return Math.max(d.total_received); })]);
-    y_total_sent_sms.domain([0, d3.max(data, function (d) { return Math.max(d.total_sent); })]);
+    y_total_received_sms.domain([0, d3.max(data, function (d) { return Math.max(d.total_received, d.golis_received, d.hormud_received, d.nationlink_received, d.somnet_received, d.somtel_received, d.telesom_received); })]);
+    y_total_sent_sms.domain([0, d3.max(data, function (d) { return Math.max(d.total_sent, d.golis_sent, d.hormud_sent, d.nationlink_sent, d.somnet_sent, d.somtel_sent, d.telesom_sent); })]);
     y_total_failed_sms.domain([0, d3.max(data, function (d) { return Math.max(d.total_errored); })]);
 
-    // Bar chart
-
     let color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    // Received bar chart
 
     // Add the Y Axis for the total received sms graph
     total_received_sms_graph.append("g")
@@ -152,15 +153,15 @@ const update = (data) => {
         .style("text-anchor", "middle")
         .text("No.of Incoming Message (s)");
 
-    let layer = total_received_sms_graph.selectAll('#receivedStack')
-        .data(dataPointsStacked)
+    let receivedLayer = total_received_sms_graph.selectAll('#receivedStack')
+        .data(receivedDataStacked)
         .enter()    
       .append('g')
         .attr('id', 'receivedStack')    
-        .attr('class', function(d, i) { return keys[i] })
+        .attr('class', function(d, i) { return receivedKeys[i] })
         .style('fill', function (d, i) { return color(i) })
     
-    layer.selectAll('rect')
+    receivedLayer.selectAll('rect')
         .data(function(d) { return d })
         .enter()
       .append('rect')
@@ -169,7 +170,7 @@ const update = (data) => {
         .attr('height', function (d) { return y_total_received_sms(d[0]) - y_total_received_sms(d[1]) })
         .attr('width', Width / Object.keys(data).length)
     
-        //Add the X Axis for the total received sms graph
+    //Add the X Axis for the total received sms graph
         total_received_sms_graph.append("g")
         .attr("transform", "translate(0," + Height + ")")
         .call(d3.axisBottom(x)
@@ -183,6 +184,39 @@ const update = (data) => {
             (Height + Margin.top + 10) + ")")
         .style("text-anchor", "middle")
         .text("Time (D:H:M:S)");
+
+    
+    // Sent bar chart
+    // Add the Y Axis for the total sent sms graph
+    total_sent_sms_graph.append("g")
+        .attr("class", "axisSteelBlue")
+        .call(d3.axisLeft(y_total_sent_sms));
+    
+    // Y axis Label for the total sent sms graph
+    total_sent_sms_graph.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - Margin.left)
+        .attr("x", 0 - (Height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("No.of Sent Message (s)");
+
+    let sentLayer = total_sent_sms_graph.selectAll('#sentStack')
+        .data(sentdDataStacked)
+        .enter()    
+      .append('g')
+        .attr('id', 'sentStack')    
+        .attr('class', function(d, i) { return sentKeys[i] })
+        .style('fill', function (d, i) { return color(i) })
+    
+    sentLayer.selectAll('rect')
+        .data(function(d) { return d })
+        .enter()
+      .append('rect')
+        .attr('x', function (d) { return x(d.data.datetime) })
+        .attr('y', function (d) { return y_total_sent_sms(d[1]) })
+        .attr('height', function (d) { return y_total_sent_sms(d[0]) - y_total_sent_sms(d[1]) })
+        .attr('width', Width / Object.keys(data).length)
     
     //Add the X Axis for the total sent sms graph
     total_sent_sms_graph.append("g")
@@ -212,6 +246,12 @@ const update = (data) => {
         .attr("dy", "1em")
         .style("text-anchor", "middle")
         .text("No.of Outgoing Message (s)");
+      
+    // update path data for total failed sms(s)
+    total_failed_path.data([data])
+        .attr("class", "line")
+        .style("stroke", "red")
+        .attr("d", total_failed_line);
 
     //Add the X Axis for the total failed sms graph
     total_failed_sms_graph.append("g")
@@ -268,4 +308,7 @@ const update = (data) => {
         .style("font-size", "20px")
         .style("text-decoration", "bold")
         .text("Total Failed Messages(s) / hr");     
+
+
+        
 };
