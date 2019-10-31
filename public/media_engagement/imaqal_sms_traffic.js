4d03037097caf15182f8c48c74f22c39b83dea44
DanielMwendwa <danielmwendwam@gmail.com>
2019-10-31 17:39:25 +0300

    Extended x-axis to accomodate the both the first & last bar on the one day range graphs

--- public/media_engagement/imaqal_sms_traffic.js
+++ public/media_engagement/imaqal_sms_traffic.js
@@ -456,8 +456,10 @@ const update = (data) => {
             yLimitReceived = yLimitReceivedTotal
         }
 
+        xMin = d3.min(data, d => new Date(d.day));
+        xMax = d3.max(data, d => new Date(d.day)).addHours(EXTEND_X_AXIS_BY_HOURS)
         // set scale domains
-        x.domain(d3.extent(dataFilteredMonth, d => new Date(d.day).addHours(EXTEND_X_AXIS_BY_HOURS)));
+        x.domain([xMin, xMax]);
         y_total_received_sms.domain([0, yLimitReceived]);
     
         d3.selectAll(".redrawElementReceived").remove();
@@ -606,8 +608,10 @@ const update = (data) => {
             yLimitSent = yLimitSentTotal
         }
 
+        xMin = d3.min(data, d => new Date(d.day));
+        xMax = d3.max(data, d => new Date(d.day)).addHours(EXTEND_X_AXIS_BY_HOURS)
         // set scale domains
-        x.domain(d3.extent(data, d => new Date(d.day).addHours(EXTEND_X_AXIS_BY_HOURS)));
+        x.domain([xMin, xMax]);
         y_total_sent_sms.domain([0, yLimitSent]);
     
         d3.selectAll(".redrawElementSent").remove();
