4d11c4d4ec93e5a945ae9e1c3252ce69f4f7e5de
Daniel Mwendwa <danielmwendwam@gmail.com>
2019-11-19 12:03:59 +0300

    initial commit

--- public/app.js
+++ public/app.js
@@ -37,23 +37,6 @@ var graphController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
-    
-    
-    return {
-        getInput: function() {
-            return {
-                type: document.querySelector(DOMstrings.inputType).value,
-            }
-            
-            // var traffics = document.getElementsByClassName('dropdown-item');
-            // for (var i = 0; i < traffics.length; i++) {
-            //     traffics[i].onclick = function () {
-            //         console.log(this.innerText)  
-            //     }
-            // }
-        }
-    }
-
 })();
 
 // // GLOBAL APP CONTROLLER
@@ -61,9 +44,10 @@ var controller = (function(authCtrl, dataCtrl, graphCtrl, UICtrl) {
     window.onload = function () {
         authCtrl.initApp();
     };
-    // authCtrl.logout()
-    var input = UICtrl.getInput();
-    console.log(input)
+    
+    document.querySelector(".btn-brown").addEventListener('click', function() {
+        authCtrl.logout()
+    });
     
 })(authController, dataController, graphController, UIController);
 
