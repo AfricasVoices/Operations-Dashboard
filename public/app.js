e13b9ab98d0961e97a3d629c7f0ae0339e8f4e4b
Daniel Mwendwa <danielmwendwam@gmail.com>
2019-11-30 20:02:05 +0300

    done

--- public/app.js
+++ public/app.js
@@ -28,52 +28,43 @@ var authController = (function() {
 // DATA CONTROLLER
 var dataController = (function() {
     var data = [];
-    var gdata = [];
-    var data2 = [];
-    var offset = new Date()
-    timerange = 30
-    offset.setDate(offset.getDate() - timerange)
+    var activeProjects = [];
+    var offset = new Date();
+    timerange = 30;
+    offset.setDate(offset.getDate() - timerange);
     var iso = d3.utcFormat("%Y-%m-%dT%H:%M:%S+%L");
-    offsetString = iso(offset)
-
-    var updateData = function(res) {
-        // Update data every time it changes in firestore
-        res.docChanges().forEach(change => {
-
-            const doc = { ...change.doc.data(), id: change.doc.id };
-
-            switch (change.type) {
-                case 'added':
-                    data2.push(doc);
-                    break;
-                case 'modified':
-                    const index = data2.findIndex(item => item.id == doc.id);
-                    data2[index] = doc;
-                    break;
-                case 'removed':
-                    data2 = data2.filter(item => item.id !== doc.id);
-                    break;
-                default:
-                    break;
-            }
-        });
-        // console.log(data2)
-    }
+    offsetString = iso(offset);
     
     return {
         resetData: function() {
             data = [];
-            gdata = [];
-            data2 = [];
+            activeProjects = [];
             operators = new Set()
         }, 
 
         getProject: function(update) {
             mediadb.collection('active_projects').onSnapshot(res => {
-                // console.log(res);
                 // Update data every time it changes in firestore
-                updateData(res)
-                update(data2);
+                res.docChanges().forEach(change => {
+
+                    const doc = { ...change.doc.data(), id: change.doc.id };
+
+                    switch (change.type) {
+                        case 'added':
+                            activeProjects.push(doc);
+                            break;
+                        case 'modified':
+                            const index = activeProjects.findIndex(item => item.id == doc.id);
+                            activeProjects[index] = doc;
+                            break;
+                        case 'removed':
+                            activeProjects = activeProjects.filter(item => item.id !== doc.id);
+                            break;
+                        default:
+                            break;
+                    }
+                });
+                update(activeProjects);
             });  
         },
 
@@ -85,7 +76,6 @@ var dataController = (function() {
 
         getCollection: function(collection, update) {   
             mediadb.collection(`/metrics/rapid_pro/${collection}/`).where("datetime", ">", offsetString).onSnapshot(res => {
-                // console.log(res)
                 // Update data every time it changes in firestore
                 res.docChanges().forEach(change => {
 
@@ -93,21 +83,20 @@ var dataController = (function() {
         
                     switch (change.type) {
                         case 'added':
-                            gdata.push(doc);
+                            data.push(doc);
                             break;
                         case 'modified':
-                            const index = gdata.findIndex(item => item.id == doc.id);
-                            gdata[index] = doc;
+                            const index = data.findIndex(item => item.id == doc.id);
+                            data[index] = doc;
                             break;
                         case 'removed':
-                            gdata = gdata.filter(item => item.id !== doc.id);
+                            data = data.filter(item => item.id !== doc.id);
                             break;
                         default:
                             break;
                     }
                 });
-                // console.log(gdata)
-                update(gdata);
+                update(data);
             });
         }
     };
@@ -167,60 +156,62 @@ var UIController = (function() {
         update_progress_ui: function(data) {
             // console.log("update_ui: " + JSON.stringify(data));
             var status_body = document.getElementById('coding_status_body');
-            while (status_body.firstChild) {
-                // status_body.removeChild(status_body.firstChild);
-            }
-            last_update = data["last_update"]
-            document.getElementById('last_update').innerText = "Last updated: " + last_update
-            for (var dataset_id in data["coding_progress"]) {
-                var messages_count = data["coding_progress"][dataset_id]["messages_count"]
-                var messages_with_label = data["coding_progress"][dataset_id]["messages_with_label"]
-                var wrong_scheme_messages = data['coding_progress'][dataset_id]['wrong_scheme_messages']
-                var not_coded_messages = data['coding_progress'][dataset_id]['not_coded_messages']
-                var dataset_link = document.createElement("a")
-                    dataset_link.setAttribute("href", "https://web-coda.firebaseapp.com/?dataset="+dataset_id)
-                    dataset_link.setAttribute('target', '_blank')
-                    dataset_link.innerText = dataset_id
-                rw = status_body.insertRow()
-                rw.insertCell().appendChild(dataset_link)
-                rw.insertCell().innerText = messages_count
-                rw.insertCell().innerText = messages_with_label
-                rw.insertCell().innerText = (100 * messages_with_label / messages_count).toFixed(2) + '%'
-                rw.insertCell().innerText = wrong_scheme_messages != null ? wrong_scheme_messages : "-"
-                rw.insertCell().innerText = wrong_scheme_messages != null ? (100 * wrong_scheme_messages / messages_count).toFixed(2) + '%' : "-"
-                rw.insertCell().innerText = not_coded_messages != null ? not_coded_messages : "-"
-                rw.insertCell().innerText = not_coded_messages != null ?(100 * not_coded_messages / messages_count).toFixed(2) + '%' : "-"
-                // console.log(dataset_id, messages_count, messages_with_label,wrong_scheme_messages,not_coded_messages);
-                //Table sorting using tablesorter plugin based on fraction of message labelling complete   
-                $("#codingtable").tablesorter({
-                    //sorting on page load, column four in descending order i.e from least coded to most coded.
-                    sortList: [[3, 0]]
-                });
-                //Trigger sorting on table data update
-                $('#codingtable').tablesorter().trigger('update');
-                //Formating rows based on cell value
-                $('#codingtable td:nth-child(4)').each(function () {
-                    var Done = $(this).text();
-                    //Style the entire row conditionally based on the cell value
-                    if ((parseFloat(Done) === 0)) {
-                        $(this).parent().addClass('coding-notstarted');
-                    }
-                    else if ((parseFloat(Done) > 0) && (parseFloat(Done) <= 25)) {
-                        $(this).parent().addClass('coding-below25');
-                    }
-                    else if ((parseFloat(Done) > 25) && (parseFloat(Done) <= 50)) {
-                        $(this).parent().addClass('coding-above25');
-                    }
-                    else if ((parseFloat(Done) > 50) && (parseFloat(Done) <= 75)) {
-                        $(this).parent().addClass('coding-above50');
-                    }
-                    else if ((parseFloat(Done) > 75) && (parseFloat(Done) < 100)) {
-                        $(this).parent().addClass('coding-above75');
-                    }
-                    else {
-                        $(this).parent().addClass('coding-complete');
-                    }
-                });
+            if (status_body) {
+                while (status_body.firstChild) {
+                    status_body.removeChild(status_body.firstChild);
+                }
+                last_update = data["last_update"]
+                document.getElementById('last_update').innerText = "Last updated: " + last_update
+                for (var dataset_id in data["coding_progress"]) {
+                    var messages_count = data["coding_progress"][dataset_id]["messages_count"]
+                    var messages_with_label = data["coding_progress"][dataset_id]["messages_with_label"]
+                    var wrong_scheme_messages = data['coding_progress'][dataset_id]['wrong_scheme_messages']
+                    var not_coded_messages = data['coding_progress'][dataset_id]['not_coded_messages']
+                    var dataset_link = document.createElement("a")
+                        dataset_link.setAttribute("href", "https://web-coda.firebaseapp.com/?dataset="+dataset_id)
+                        dataset_link.setAttribute('target', '_blank')
+                        dataset_link.innerText = dataset_id
+                    rw = status_body.insertRow()
+                    rw.insertCell().appendChild(dataset_link)
+                    rw.insertCell().innerText = messages_count
+                    rw.insertCell().innerText = messages_with_label
+                    rw.insertCell().innerText = (100 * messages_with_label / messages_count).toFixed(2) + '%'
+                    rw.insertCell().innerText = wrong_scheme_messages != null ? wrong_scheme_messages : "-"
+                    rw.insertCell().innerText = wrong_scheme_messages != null ? (100 * wrong_scheme_messages / messages_count).toFixed(2) + '%' : "-"
+                    rw.insertCell().innerText = not_coded_messages != null ? not_coded_messages : "-"
+                    rw.insertCell().innerText = not_coded_messages != null ?(100 * not_coded_messages / messages_count).toFixed(2) + '%' : "-"
+                    // console.log(dataset_id, messages_count, messages_with_label,wrong_scheme_messages,not_coded_messages);
+                    //Table sorting using tablesorter plugin based on fraction of message labelling complete   
+                    $("#codingtable").tablesorter({
+                        //sorting on page load, column four in descending order i.e from least coded to most coded.
+                        sortList: [[3, 0]]
+                    });
+                    //Trigger sorting on table data update
+                    $('#codingtable').tablesorter().trigger('update');
+                    //Formating rows based on cell value
+                    $('#codingtable td:nth-child(4)').each(function () {
+                        var Done = $(this).text();
+                        //Style the entire row conditionally based on the cell value
+                        if ((parseFloat(Done) === 0)) {
+                            $(this).parent().addClass('coding-notstarted');
+                        }
+                        else if ((parseFloat(Done) > 0) && (parseFloat(Done) <= 25)) {
+                            $(this).parent().addClass('coding-below25');
+                        }
+                        else if ((parseFloat(Done) > 25) && (parseFloat(Done) <= 50)) {
+                            $(this).parent().addClass('coding-above25');
+                        }
+                        else if ((parseFloat(Done) > 50) && (parseFloat(Done) <= 75)) {
+                            $(this).parent().addClass('coding-above50');
+                        }
+                        else if ((parseFloat(Done) > 75) && (parseFloat(Done) < 100)) {
+                            $(this).parent().addClass('coding-above75');
+                        }
+                        else {
+                            $(this).parent().addClass('coding-complete');
+                        }
+                    });
+                }
             }
         }, 
 
@@ -273,12 +264,14 @@ var controller = (function(authCtrl, dataCtrl, graphCtrl, UICtrl) {
         authCtrl.logout()
     };
 
+    // Navigate to coding progress page
     var ctrlDisplayCodingProgress = function(e) {
         if(e.target && e.target.nodeName == "A") {
             window.location.reload();
         }
     };
     
+    // Navigate to the selected project graphs
     var ctrlDisplayProject = function(e) {
         var collection;
         var DOM = UICtrl.getDOMstrings();
@@ -307,9 +300,11 @@ var controller = (function(authCtrl, dataCtrl, graphCtrl, UICtrl) {
     };
     
 })(authController, dataController, graphController, UIController);
+// initialize firestore
 const mediadb = firebase.firestore();
 const settings = { timestampsInSnapshots: true };
 mediadb.settings(settings);
+// initialize the app 
 controller.init();
 
 
