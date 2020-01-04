f8ee7f66bbfd01d3b6fa71192c7bfd63738526e8
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-01-04 21:03:10 +0300

    class implementation of data controller

--- public/data.js
+++ public/data.js
@@ -1,28 +1,20 @@
 // DATA CONTROLLER
-var dataController = (function() {
-    var data = [];
-    var activeProjects = [];
-    var offset = new Date();
-    timerange = 30;
-    offset.setDate(offset.getDate() - timerange);
-    var iso = d3.utcFormat("%Y-%m-%dT%H:%M:%S+%L");
-    offsetString = iso(offset);
-
-    var updateData = function(response, input) {
+class DataController {
+    static updateData = function(response, input) {
         // Update data every time it changes in firestore
         response.docChanges().forEach(change => {
 
             const doc = { ...change.doc.data(), id: change.doc.id };
 
             switch (change.type) {
-                case 'added':
+                case "added":
                     input.push(doc);
                     break;
-                case 'modified':
+                case "modified":
                     const index = input.findIndex(item => item.id == doc.id);
                     input[index] = doc;
                     break;
-                case 'removed':
+                case "removed":
                     input = input.filter(item => item.id !== doc.id);
                     break;
                 default:
@@ -30,34 +22,33 @@ var dataController = (function() {
             }
         });
     }
-    
-    return {
-        resetData: function() {
-            data = [];
-            activeProjects = [];
-            operators = new Set()
-        }, 
 
-        getProject: function(update) {
-            mediadb.collection('active_projects').onSnapshot(res => {
-                // Update data every time it changes in firestore
-                updateData(res, activeProjects);
-                update(activeProjects);
-            });  
-        },
+    static getProject(update) {
+        let activeProjects = []
+        mediadb.collection('active_projects').onSnapshot(res => {
+            // Update data every time it changes in firestore
+            DataController.updateData(res, activeProjects);
+            update(activeProjects);
+        });  
+    }
 
-        getDocument: function(update) {
-            mediadb.doc('metrics/coda').onSnapshot(function(res) {
-                update(res.data());
-            });
-        },
+    static getDocument(update) {
+        mediadb.doc('metrics/coda').onSnapshot(res => {
+            update(res.data());
+        });
+    }
 
-        getCollection: function(collection, update) {   
-            mediadb.collection(`/metrics/rapid_pro/${collection}/`).where("datetime", ">", offsetString).onSnapshot(res => {
-                // Update data every time it changes in firestore
-                updateData(res, data);
-                update(data);
-            });
-        }
-    };
-})();
+    static getCollection(collection, update) {  
+        let data = [];
+        let offset = new Date();
+        const timerange = 30 
+        offset.setDate(offset.getDate() - timerange);
+        let iso = d3.utcFormat("%Y-%m-%dT%H:%M:%S+%L");
+        let offsetString = iso(offset)
+        mediadb.collection(`/metrics/rapid_pro/${collection}/`).where("datetime", ">", offsetString).onSnapshot(res => {
+            // Update data every time it changes in firestore
+            DataController.updateData(res, data);
+            update(data);
+        });
+    }
+}
