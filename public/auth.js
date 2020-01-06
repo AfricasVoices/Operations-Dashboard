c7d5d0009f16a9ad2b91f799ce60cb81cd54551d
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-01-06 11:11:01 +0300

    added auth js to handle authentication

---
+++ public/auth.js
@@ -0,0 +1,20 @@
+class AuthController {
+    // Authentication state listener
+    static get_user() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                console.log("User Authorized");
+            } else {
+                window.location.replace("auth.html")
+            }
+        });
+    }
+
+    static logout() {
+        firebase.auth().signOut()
+            .catch(err => {
+                alert("An error occurred")
+                console.log(err);
+            })
+    }
+}
