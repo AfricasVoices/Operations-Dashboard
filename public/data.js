61d07c5dc21919fe5aa1e62e4d1d2dfdea2adf87
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-01-10 11:26:40 +0300

    formatted data js code

--- public/data.js
+++ public/data.js
@@ -22,38 +22,41 @@ class DataController {
     }
 
     static watchActiveProjects(onChange) {
-        let activeProjects = []
-        mediadb.collection('active_projects').onSnapshot(res => {
+        let activeProjects = [];
+        mediadb.collection("active_projects").onSnapshot(res => {
             // Update data every time it changes in firestore
             DataController.updateData(res, activeProjects);
-                onChange(activeProjects);
-        });  
+            onChange(activeProjects);
+        });
     }
 
     static watchCodingProgress(onChange, hasListener = true) {
-        let unsubscribe = mediadb.doc('metrics/coda').onSnapshot(res => {
+        let unsubscribe = mediadb.doc("metrics/coda").onSnapshot(res => {
             onChange(res.data());
         });
         if (!hasListener) {
-            unsubscribe()
+            unsubscribe();
         }
     }
 
-    static watchProjectTrafficData(projectName, onChange, hasListener = true) {  
-        let data = [];
+    static watchProjectTrafficData(projectName, onChange, hasListener = true) {
+        const TIMERANGE = 30;
         let offset = new Date();
-        const TIMERANGE = 30 
         offset.setDate(offset.getDate() - TIMERANGE);
-        let iso = d3.utcFormat("%Y-%m-%dT%H:%M:%S+%L");
-        let offsetString = iso(offset)
-        let projectCollection = projectName
-        let unsubscribe = mediadb.collection(`/metrics/rapid_pro/${projectCollection}/`).where("datetime", ">", offsetString).onSnapshot(res => {
-            // Update data every time it changes in firestore
-            DataController.updateData(res, data);
-            onChange(data);
-        });
+        let data = [],
+            iso = d3.utcFormat("%Y-%m-%dT%H:%M:%S+%L"),
+            offsetString = iso(offset),
+            projectCollection = projectName,
+            unsubscribe = mediadb
+                .collection(`/metrics/rapid_pro/${projectCollection}/`)
+                .where("datetime", ">", offsetString)
+                .onSnapshot(res => {
+                    // Update data every time it changes in firestore
+                    DataController.updateData(res, data);
+                    onChange(data);
+                });
         if (!hasListener) {
-            unsubscribe()
+            unsubscribe();
         }
     }
 }
