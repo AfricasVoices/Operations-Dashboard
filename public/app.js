3d191fa819765cb40d9012172873ec2bf9201fee
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-01-10 11:46:03 +0300

    formatted app js code

--- public/app.js
+++ public/app.js
@@ -2,10 +2,16 @@
 class Controller {
     static setupEventListeners() {
         let DOMstrings = UIController.getDOMstrings();
-        document.querySelector(DOMstrings.logoutBtn).addEventListener('click', AuthController.logout);
-        document.querySelector(DOMstrings.codingProgressLinkSelector).addEventListener('click', Controller.navigateToCodingProgress);
-        document.querySelector(DOMstrings.projectMenu).addEventListener('click', Controller.navigateToSelectedProject);          
-    };
+        document
+            .querySelector(DOMstrings.logoutBtn)
+            .addEventListener("click", AuthController.logout);
+        document
+            .querySelector(DOMstrings.codingProgressLinkSelector)
+            .addEventListener("click", Controller.navigateToCodingProgress);
+        document
+            .querySelector(DOMstrings.projectMenu)
+            .addEventListener("click", Controller.navigateToSelectedProject);
+    }
 
     static resetUI() {
         let DOMstrings = UIController.getDOMstrings();
@@ -14,13 +20,18 @@ class Controller {
     }
 
     static detachSnapshotListener() {
-        let DOMstrings = UIController.getDOMstrings(), hasListener = false;
-        let nodeList =  document.querySelectorAll(DOMstrings.dropdownItem);
+        let DOMstrings = UIController.getDOMstrings(),
+            hasListener = false,
+            nodeList = document.querySelectorAll(DOMstrings.dropdownItem);
         if (nodeList) {
-            // Makes sure all the snapshot listeners of the projects' traffic data are detached 
+            // Makes sure all the snapshot listeners of the projects' traffic data are detached
             Array.prototype.forEach.call(nodeList, obj => {
-                let project = obj.innerText
-                DataController.watchProjectTrafficData(project, GraphController.updateGraphs, hasListener);
+                let project = obj.innerText;
+                DataController.watchProjectTrafficData(
+                    project,
+                    GraphController.updateGraphs,
+                    hasListener
+                );
             });
         }
         // detach coda snapshot listener
@@ -28,31 +39,31 @@ class Controller {
     }
 
     static navigateToCodingProgress(e) {
-        if(e.target && e.target.nodeName == "A") {
-            Controller.detachSnapshotListener()
-            Controller.resetUI()
+        if (e.target && e.target.nodeName == "A") {
+            Controller.detachSnapshotListener();
+            Controller.resetUI();
             // Add the coding progress section to the UI
             UIController.addCodingProgressSection();
             // Get data for coding progress table
             DataController.watchCodingProgress(UIController.updateProgressUI);
         }
-    };
-    
+    }
+
     static navigateToSelectedProject(e) {
-        if(e.target && e.target.nodeName == "A") {
-            Controller.detachSnapshotListener()
-            Controller.resetUI()
-            console.log(e.target.innerText)
-            let project = e.target.innerText
-             // Add the graphs container to the UI
+        if (e.target && e.target.nodeName == "A") {
+            Controller.detachSnapshotListener();
+            Controller.resetUI();
+            console.log(e.target.innerText);
+            let project = e.target.innerText;
+            // Add the graphs container to the UI
             UIController.addGraphs(project);
             // Update and show the Graphs
             DataController.watchProjectTrafficData(project, GraphController.updateGraphs);
         }
-    };  
+    }
 
     static init() {
-        console.log('Application has started.');
+        console.log("Application has started.");
         // Authorize user
         AuthController.getUser();
         // set up event listeners
@@ -63,11 +74,11 @@ class Controller {
         UIController.addCodingProgressSection();
         // Get data for coding progress table
         DataController.watchCodingProgress(UIController.updateProgressUI);
-    } 
-} 
+    }
+}
 
 // Initialize the application
-const mediadb = firebase.firestore();
-const settings = {timestampsInSnapshots: true};
+const mediadb = firebase.firestore(),
+    settings = { timestampsInSnapshots: true };
 mediadb.settings(settings);
 Controller.init();
