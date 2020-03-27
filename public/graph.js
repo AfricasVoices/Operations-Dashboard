0a78d938a58c0a9ef773104f2105d1c95846ccb2
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-03-27 15:04:10 +0300

    updated 10 min failed graph ticks and ticks format

--- public/graph.js
+++ public/graph.js
@@ -418,8 +418,8 @@ class GC {
             .attr("class", "redrawElementFailed")
             .attr("transform", "translate(0," + GC.Height + ")")
             .call(d3.axisBottom(GC.failed_messages_x_axis_range)
-                    .ticks(5)
-                    .tickFormat(GC.dayDateFormat))
+                .ticks(d3.timeDay.every(1))
+                .tickFormat(GC.dayDateFormat))
             // Rotate axis labels
             .selectAll("text")
             .style("text-anchor", "end")
