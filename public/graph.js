ab785e30d8b07fc309e506443d4497b45b83db70
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-04-23 15:20:56 +0300

    adjust x and width attributes of one day sent bars

--- public/graph.js
+++ public/graph.js
@@ -651,16 +651,17 @@ class GraphController {
                 .attr("class", (d, i) => sentKeys[i])
                 .style("fill", (d, i) => color(i));
 
-            let innerPadding = 5;
+            // Values to adjust x and width attributes
+            let rightPadding = -2, shiftBarsToRight = 1;
             sentLayer
                 .selectAll("rect")
                 .data(d => d)
                 .enter()
                 .append("rect")
-                .attr("x", d => x(new Date(d.data.day)))
+                .attr("x", d => x(new Date(d.data.day)) + shiftBarsToRight)
                 .attr("y", d => y_total_sent_sms_range(d[1]))
                 .attr("height", d => y_total_sent_sms_range(d[0]) - y_total_sent_sms_range(d[1]))
-                .attr("width", (Width / Object.keys(dailySentTotal).length) - innerPadding);
+                .attr("width", (Width / Object.keys(dailySentTotal).length) + rightPadding);
 
             // Add tooltip for the total sent sms graph
             let tip;
