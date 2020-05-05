6f2193234b4a21feb3930b7f61b719fcd4bee904
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-05-05 14:55:18 +0300

    compute x axis tick values for 10 min sent graph

--- public/graph.js
+++ public/graph.js
@@ -606,6 +606,26 @@ class GraphController {
             d3.selectAll("#sentStack1day").remove();
             d3.selectAll("#sentStack10min").remove();
 
+            // Group data filtered by week daily and generate tick values for x axis
+            let dataFilteredWeekGroupedDaily  = d3.nest().key(d => d.day)
+                .rollup(v => {
+                    let firstTimestampOfDay = {}
+                    firstTimestampOfDay["datetime"] = d3.min(v,d => d.datetime)
+                    return firstTimestampOfDay
+                })
+                .entries(dataFilteredWeek);
+
+            // Flatten nested data
+            for (let entry in dataFilteredWeekGroupedDaily) {
+                let valueList = dataFilteredWeekGroupedDaily[entry].value;
+                for (let key in valueList) {
+                    dataFilteredWeekGroupedDaily[entry][key] = valueList[key];
+                }
+                delete dataFilteredWeekGroupedDaily[entry]["value"];
+                delete dataFilteredWeekGroupedDaily[entry]["key"];
+            }
+            const tickValuesForXAxis = dataFilteredWeekGroupedDaily.map(d => d.datetime);
+
             // Add the Y Axis for the total sent sms graph
             total_sent_sms_graph
                 .append("g")
