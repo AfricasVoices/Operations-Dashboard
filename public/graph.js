28adc2841a29e3da0b43377c8b267ee7b4fb41b5
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-05-05 14:57:46 +0300

    compute x axis tick values for 10 min failed graph

--- public/graph.js
+++ public/graph.js
@@ -975,6 +975,26 @@ class GraphController {
             d3.selectAll("#failedBarChart").remove();
             d3.selectAll("#failedBarChart10min").remove();
 
+            // Group data filtered by week daily and generate tick values for x axis
+            let dataFilteredWeekGroupedDaily  = d3.nest().key(d => d.day)
+                .rollup(v => {
+                    let firstTimestampOfDay = {}
+                    firstTimestampOfDay["datetime"] = d3.min(v,d => d.datetime)
+                    return firstTimestampOfDay
+                })
+                .entries(dataFilteredWeek);
+
+            // Flatten nested data
+            for (let entry in dataFilteredWeekGroupedDaily) {
+                let valueList = dataFilteredWeekGroupedDaily[entry].value;
+                for (let key in valueList) {
+                    dataFilteredWeekGroupedDaily[entry][key] = valueList[key];
+                }
+                delete dataFilteredWeekGroupedDaily[entry]["value"];
+                delete dataFilteredWeekGroupedDaily[entry]["key"];
+            }
+            const tickValuesForXAxis = dataFilteredWeekGroupedDaily.map(d => d.datetime);
+
             // Add the Y Axis for the total failed sms graph
             total_failed_sms_graph
                 .append("g")
