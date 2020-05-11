8d2997de637aedf4700cb1d1b994bfa77194da4e
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-05-11 11:46:14 +0300

    ops test instance app's Firebase configuration

--- public/firebase.js
+++ public/firebase.js
@@ -1,10 +1,11 @@
 let config = {
-    apiKey: "AIzaSyCUHQE4gDk35-BtZrlNEkNbr1nX2Yn4l5Q",
-    authDomain: "avf-dashboards.firebaseapp.com",
-    databaseURL: "https://avf-dashboards.firebaseio.com",
-    projectId: "avf-dashboards",
-    storageBucket: "avf-dashboards.appspot.com",
-    messagingSenderId: "1063777163571",
-    appId: "1:1063777163571:web:57361756b9ecb959"
+    apiKey: "AIzaSyCUm5b5RNG4DbQLwE68lyYsRa2LEW2M1JE",
+    authDomain: "avf-dashboards-test.firebaseapp.com",
+    databaseURL: "https://avf-dashboards-test.firebaseio.com",
+    projectId: "avf-dashboards-test",
+    storageBucket: "avf-dashboards-test.appspot.com",
+    messagingSenderId: "5161953516",
+    appId: "1:5161953516:web:06e49f95b37ad0d6e89dc9",
+    measurementId: "G-BQLFD256EE"
 };
 firebase.initializeApp(config);
