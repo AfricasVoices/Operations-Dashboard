9c9f9c9797f5690b5ace8dd613fe0268d71f49e7
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-05-19 16:06:31 +0300

    set the dimensions and margins of the graphs

--- public/systems_graphs.js
+++ public/systems_graphs.js
@@ -6,5 +6,10 @@ class SystemGraphsController {
         // Clear previous graphs before redrawing
         d3.selectAll("svg").remove();
 
+        // Set the dimensions and margins of the graphs
+        const Margin = {top: 40, right: 100, bottom: 105, left: 70},
+            Width = 960 - Margin.left - Margin.right,
+            Height = 500 - Margin.top - Margin.bottom;
+
     }
 }
