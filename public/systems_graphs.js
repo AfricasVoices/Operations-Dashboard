d1e8cd47ccc5feb131995436b6924792521230e2
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-05-19 16:05:33 +0300

    Clear previous graphs before redrawing

--- public/systems_graphs.js
+++ public/systems_graphs.js
@@ -3,5 +3,8 @@ class SystemGraphsController {
 
         let dayTimeFormat = d3.timeFormat("%a %d (%H:%M)");
 
+        // Clear previous graphs before redrawing
+        d3.selectAll("svg").remove();
+
     }
 }
