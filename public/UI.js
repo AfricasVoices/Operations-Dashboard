b3c2bf745b6f203948ff79d8834b6b3218176f34
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-05-19 15:44:07 +0300

    function to add system graph elements into DOM

--- public/UI.js
+++ public/UI.js
@@ -185,4 +185,22 @@ class UIController {
             .querySelector(DOMstrings.codingProgressContainer)
             .insertAdjacentHTML("beforeend", newHtml);
     }
+
+    static addSystemsGraphs() {
+        let DOMstrings = UIController.getDOMstrings(),
+            html = `<div class="container"> 
+            <section>
+                <div class="card shadow chart my-1"></div>
+            </section> 
+            <section>
+                <div class="card shadow chart2 my-1"></div>
+            </section> 
+            <section>
+                <div class="card shadow chart3 my-1"></div>
+            </section> 
+        </div> `;
+        document
+            .querySelector(DOMstrings.codingProgressContainer)
+            .insertAdjacentHTML("beforeend", html);
+    }
 }
