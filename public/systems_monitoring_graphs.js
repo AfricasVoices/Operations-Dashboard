a58506126aa0b6cc38bb46a60e62449367f70837
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-05-20 16:04:25 +0300

    generate legend for the graph

--- public/systems_monitoring_graphs.js
+++ public/systems_monitoring_graphs.js
@@ -534,6 +534,34 @@ class SystemGraphsController {
                 d3.selectAll(".cpuArea").style("opacity", 1)
             }
 
+            // Add legend for each name.
+            let size = 20
+            svg.selectAll("myrect")
+                .data(cpuKeys)
+                .enter()
+                .append("rect")
+                    .attr("x", Width + 10)
+                    .attr("y", (d,i) => 10 + i*(size+5)) // 10 is where the first dot appears. 25 is the distance between dots
+                    .attr("width", size)
+                    .attr("height", size)
+                    .style("fill", d => color(d))
+                    .on("mouseover", highlight)
+                    .on("mouseleave", noHighlight)
+
+            // Add legend label for each name.
+            svg.selectAll("mylabels")
+                .data(cpuMetrics)
+                .enter()
+                .append("text")
+                    .attr("x", Width + 10 + size*1.2)
+                    .attr("y", (d,i) => 10 + i*(size+5) + (size/2)) // 10 is where the first dot appears. 25 is the distance between dots
+                    .style("fill", d => color(d))
+                    .text(d => d)
+                    .attr("text-anchor", "left")
+                    .style("alignment-baseline", "middle")
+                    .on("mouseover", highlight)
+                    .on("mouseleave", noHighlight)
+
         }
 
     }
