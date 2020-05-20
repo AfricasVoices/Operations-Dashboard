17118dc06aa7fa3e92d5c536c0e9fefddf4c7b7c
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-05-20 13:25:57 +0300

    add new line

--- public/systems_monitoring_graphs.js
+++ public/systems_monitoring_graphs.js
@@ -211,7 +211,7 @@ class SystemGraphsController {
                 memoryStr =  "memory_" + memoryMetrics[i];
                 memoryKeys.push(memoryStr);
             }
-            
+
             let colors = ["#4B0082", "#fbdb9c"]
             // color palette
             let color = d3.scaleOrdinal()
