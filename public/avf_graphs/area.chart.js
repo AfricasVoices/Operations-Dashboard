6ce3b1c68a65030ae65200fa7225bf64386d1fe9
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-05-26 11:19:35 +0300

    add constructor function

--- public/avf_graphs/area.chart.js
+++ public/avf_graphs/area.chart.js
@@ -1,5 +1,17 @@
 import { GraphLayout } from "./graph.layout.js";
 
 export class AreaChart extends GraphLayout {
-    
+
+    constructor(opts) {
+        // Load in arguments from config object
+        super(opts.element)
+        this.data = opts.data;
+        this.id = "id";
+        this.color = "red";
+        this.title = "Area Chart";
+        this.xAxisLabel = "X Axis";
+        this.yAxisLabel = "Y Axis";
+
+        this.draw()
+    }
 }
