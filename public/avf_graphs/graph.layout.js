8dc746e2e29a4c549ea1e36f44f77867a57c7ff4
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-05-26 11:07:15 +0300

    function to generate layout of graphs

--- public/avf_graphs/graph.layout.js
+++ public/avf_graphs/graph.layout.js
@@ -2,4 +2,26 @@ export class GraphLayout {
     constructor(element) {
         this.element = element;
     }
+
+    layout() {
+        // Define width, height and margin
+        this.margin = {
+            top: 40,
+            right: 100,
+            bottom: 105,
+            left: 70
+        };
+        this.width = 960 - this.margin.left - this.margin.right;
+        this.height = 500 - this.margin.top - this.margin.bottom;
+
+        // Set up parent element and SVG
+        this.element.innerHTML = '';
+        const svg = d3.select(this.element).append('svg');
+        svg.attr('width',  this.width + this.margin.left + this.margin.right);
+        svg.attr('height', this.height +  this.margin.top + this.margin.bottom);
+
+        // We'll actually be appending to a <g> element
+        this.plot = svg.append('g')
+            .attr('transform',`translate(${this.margin.left},${this.margin.top})`);
+    }
 }
