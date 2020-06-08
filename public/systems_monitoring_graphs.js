be87f4fd2a4fafdb9cd2dc437e0e4f0e0c258906
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-06-08 17:06:07 +0300

    plot disk usage graph

--- public/systems_monitoring_graphs.js
+++ public/systems_monitoring_graphs.js
@@ -6,6 +6,26 @@ export class SystemGraphsController {
         // Clear previous graphs before redrawing
         d3.selectAll("svg").remove();
 
+/*                         DISK UTILIZATION GRAPH
+/*      ==================================================================
+*/      let diskUsageChartData = JSON.parse(JSON.stringify(data));
+        diskUsageChartData.forEach(function(d) {
+            d.datetime = new Date(d.datetime);
+            d.value = +d.disk_usage.used;
+        })
+
+        const area3 = new AreaChart(
+            {element: document.querySelector('.disc-usage-chart'), data: diskUsageChartData });
+        area3
+            .setId("disk")
+            .setTitle("Disk Utilization")
+            .setXAxisLabel("Date (dd:hh:m)")
+            .setYAxisLabel("Disk Usage (GB)")
+            .setColorScheme("#0E86D4")
+            .setYLimit(diskUsageChartData[0].disk_usage.total)
+            .setFeatureInAnalysis("system-metrics")
+            .draw();
+
         let fullDateFormat = d3.timeFormat("%Y-%m-%d %H:%M:%S");
         // Update timestamp of update and reset formatting
         let lastUpdateTimeStamp = new Date(
