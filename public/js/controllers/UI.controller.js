8a04fbd84329ffec0beb98db0c6a6f70d05d661e
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-06-18 15:55:27 +0300

    Add space after comma
    Co-authored-by: Alexander Simpson <as2388@cam.ac.uk>

--- public/js/controllers/UI.controller.js
+++ public/js/controllers/UI.controller.js
@@ -109,7 +109,7 @@ export class UIController {
     static addGraphs(title) {
         let DOMstrings = UIController.getDOMstrings(),
             script = document.createElement('script');
-        script.setAttribute('src','js/libs/scroll.js');
+        script.setAttribute('src', 'js/libs/scroll.js');
         document.head.appendChild(script);
         let html = `<div class="container"> 
             <div class="d-md-flex justify-content-between p-1">
