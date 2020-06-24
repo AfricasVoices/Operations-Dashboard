f93abffee663a6a80347b004867f16804f9d52d7
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-06-24 16:07:21 +0300

    update mno color scheme to use color for unspecified operator in MNO colors

--- public/js/controllers/traffic_graphs_controller.js
+++ public/js/controllers/traffic_graphs_controller.js
@@ -210,6 +210,10 @@ export class TrafficGraphsController {
         let mno_color_scheme = [],
             operators_with_color_identity = Object.keys(MNOColors);
 
+        let difference = operators.filter(x => !operators_with_color_identity.includes(x))[0];
+        let difference_index = operators.indexOf(difference);
+        mno_color_scheme[difference_index] = MNOColors.other;
+
         // Generate color scheme based on operators identity
         operators.forEach((operator, index) => {
             if (operators_with_color_identity.includes(operator)) {
