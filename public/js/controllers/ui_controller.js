2116d1d6e82e867eaf43e3f5d8f8ced45fcdd109
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-06-29 17:27:32 +0300

    add the shadow-none Bootstrap class to input elements

--- public/js/controllers/ui_controller.js
+++ public/js/controllers/ui_controller.js
@@ -137,21 +137,21 @@ export class UIController {
             <section>
                 <div class="d-md-flex justify-content-start my-2">
                     <span class="font-weight-bold" type="text">Set the maximum number of incoming messages you want to see</span> 
-                    <div class="col-md-2"><input class="form-control form-control-sm" type="number" id="buttonYLimitReceived" step="100" min="10"></div>
+                    <div class="col-md-2"><input class="form-control form-control-sm shadow-none" type="number" id="buttonYLimitReceived" step="100" min="10"></div>
                 </div>
                 <div class="card shadow total_received_sms_graph"></div>
             </section> 
             <section>
                 <div class="d-md-flex justify-content-start mt-4 mb-3">
                     <span class="font-weight-bold" type="text">Set the maximum number of outgoing messages you want to see</span> 
-                    <div class="col-md-2"><input class="form-control form-control-sm" type="number" id="buttonYLimitSent" step="500" min="10"></div>
+                    <div class="col-md-2"><input class="form-control form-control-sm shadow-none" type="number" id="buttonYLimitSent" step="500" min="10"></div>
                 </div>
                 <div class="card shadow total_sent_sms_graph"></div>
             </section> 
             <section>
                 <div class="d-md-flex justify-content-start mt-4 mb-3">
                     <span class="font-weight-bold" type="text">Set the maximum number of failed messages you want to see</span> 
-                    <div class="col-md-2"><input class="form-control form-control-sm" type="number" id="buttonYLimitFailed" step="50" min="10"></div>
+                    <div class="col-md-2"><input class="form-control form-control-sm shadow-none" type="number" id="buttonYLimitFailed" step="50" min="10"></div>
                 </div>
                 <div class="card shadow total_failed_sms_graph my-4"></div> 
             </section>
