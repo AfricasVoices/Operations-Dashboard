0b0ef7110de69ed4e2538a45e7a954c80de53c70
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-07-07 15:50:21 +0300

    add function to create scales

--- public/js/libs/bar_chart.js
+++ public/js/libs/bar_chart.js
@@ -47,4 +47,24 @@ export class BarChart extends GraphLayout {
         this.xLimit = xLimit;
         return this;
     }
+
+    createScales() {
+        // Calculate max and min for data
+        const xExtent = d3.extent(this.data, d => new Date(d.datetime));
+        const yExtent = this.yLimit ? [0, this.yLimit] : d3.extent(this.data, d => +d.value);
+
+        // Force zero baseline if all data points are positive
+        if (yExtent[0] > 0) { yExtent[0] = 0; };
+
+        if (this.xLimit) { xExtent[1] = this.xLimit; }
+
+        this.xScale = d3.scaleTime()
+            .range([1, this.width])
+            .domain(xExtent);
+
+        this.yScale = d3.scaleLinear()
+            .range([this.height, 0]);
+
+        if (yExtent[1] > 0) { this.yScale.domain(yExtent) };
+    }
 }
