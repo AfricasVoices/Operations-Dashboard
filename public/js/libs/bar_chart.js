30fb0ac3344133b5e02b7198afc4a23a1b238ede
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-07-07 15:39:28 +0300

    add BarChart constructor

--- public/js/libs/bar_chart.js
+++ public/js/libs/bar_chart.js
@@ -1,5 +1,20 @@
 import { GraphLayout } from "./graph_layout.js";
 
 export class BarChart extends GraphLayout {
-    
+    constructor(opts) {
+        // Load in arguments from config object
+        super(opts.element);
+        this.data = opts.data;
+        this.id = "id";
+        this.color = "red";
+        this.title = "Bar Chart";
+        this.xAxisLabel = "X Axis";
+        this.yAxisLabel = "Y Axis";
+        this.config = {
+            // Failed Messages Graph Configuration
+            setFailedMsgGraphTooltipText: false,
+        };
+
+        this.draw();
+    }
 }
