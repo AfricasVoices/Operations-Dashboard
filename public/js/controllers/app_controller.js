e6acc53129194af5e44c6a95ffcbb9ddcae823b4
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-09-10 06:46:31 +0300

    revoke clearAllTimers where appropriate

--- public/js/controllers/app_controller.js
+++ public/js/controllers/app_controller.js
@@ -87,6 +87,7 @@ class Controller {
 
     static navigateToCodingProgress(e) {
         if (e.target && e.target.nodeName == "A") {
+            Controller.clearAllTimers();
             DataController.detachSnapshotListener();
             window.location.hash = "coding_progress";
             Controller.displayCodingProgress();
@@ -95,9 +96,7 @@ class Controller {
 
     static navigateToSelectedProject(e) {
         if (e.target && e.target.nodeName == "A") {
-            import("./traffic_graphs_controller.js").then(module => {
-                module.TrafficGraphsController.clearTimers();
-            });
+            Controller.clearAllTimers();
             DataController.detachSnapshotListener();
             console.log(e.target.innerText);
             let project = e.target.innerText;
@@ -108,9 +107,7 @@ class Controller {
 
     static navigateToSystems(e) {
         if (e.target && e.target.nodeName == "A") {
-            import("./systems_graphs_controller.js").then(module => {
-                module.SystemsGraphsController.clearTimers();
-            });
+            Controller.clearAllTimers();
             DataController.detachSnapshotListener();
             window.location.hash = "systems";
             Controller.displaySystems();
