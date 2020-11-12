8a37a04a5633df4c927d6cc1720cdc5934423bab
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-11-12 23:37:35 +0300

    update pipeline metrics table and dot plot

--- public/js/controllers/pipelines_controller.js
+++ public/js/controllers/pipelines_controller.js
@@ -1,11 +1,11 @@
 export class PipelinesController {
     static updatePipelinePage(data) {
-        PipelinesController.updateGraphs(data.pipelineMetrics)
+        PipelinesController.updateGraphs(data.dt, data.pipelineMetrics)
         PipelinesController.updatePipelineProgressTable(data.pipelineProgressTableData);
     }
 
-    static updateGraphs(data) {
-        console.log(data);
+    static updateGraphs(data, metrics) {
+        console.log(data)
         // Clear previous graphs before redrawing
         d3.selectAll("svg").remove();
 
@@ -24,20 +24,170 @@ export class PipelinesController {
         
         // Add X axis
         let x = d3.scaleTime()
-            .domain(d3.extent(data, d => new Date(d.timestamp)))
+            .domain(d3.extent(metrics, d => d.timestamp))
             .range([0, Width]);
-        svg.append("g")
+        let dayTimeFormat = d3.timeFormat("%a %d (%H:%M)");
+        let xAxis = svg.append("g")
             .attr("transform", "translate(0," + Height + ")")
-            .call(d3.axisBottom(x))
+            .call(d3.axisBottom(x).ticks(d3.timeDay.every(1)).tickFormat(dayTimeFormat))
+        // Rotate axis labels
+        xAxis.selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-65)");
 
         // Y axis
         let y = d3.scaleBand()
             .range([ 0, Height ])
-            .domain(data.map(function(d) { return d.project; }))
+            .domain(data.map(d => d.project))
             .padding(1);
         svg.append("g")
             .call(d3.axisLeft(y));
 
+        // Add a clipPath: everything out of this area won't be drawn.
+        let clip = svg.append("defs").append("svg:clipPath")
+            .attr("id", "clip")
+            .append("svg:rect")
+            .attr("width", Width)
+            .attr("height", Height )
+            .attr("x", 0)
+            .attr("y", 0);
+
+        // Add brushing
+        let brush = d3.brushX()                   
+            .extent( [ [0,0], [Width, Height] ] )  
+            .on("end", updateChart)               
+
+        // Create the line variable: where both the line and the brush take place
+        let plot = svg.append('g')
+            .attr("clip-path", "url(#clip)")
+
+        // Lines
+        plot.selectAll("myline")
+            .data(data)
+            .enter()
+            .append("line")
+                .attr("class", "line")
+                .attr("x1", d => x(d.value1))
+                .attr("x2", d => {
+                    if (d.running) {
+                        return x(d.value1)
+                    } else {
+                        return x(d.value2);
+                    }
+                })
+                .attr("y1", d => y(d.project))
+                .attr("y2", d => y(d.project))
+                .attr("stroke", d => {
+                    if (d.failed) {
+                        return "red";
+                    } else if (d.success) {
+                        return "green";
+                    } else {
+                        return "blue";
+                    }
+                })
+                .attr("stroke-width", "2px");
+
+        // Circles of variable 1
+        plot.selectAll("mycircle")
+            .data(data)
+            .enter()
+            .append("circle")
+                .attr("class", "circle1")
+                .attr("cx", d => x(d.value1))
+                .attr("cy", d => y(d.project))
+                .attr("r", "6")
+                .style("fill", "#69b3a2");
+
+        // Circles of variable 2
+        plot.selectAll("mycircle")
+            .data(data)
+            .enter()
+            .append("circle")
+                .attr("class", "circle2")
+                .attr("cx", d => {
+                    if (d.running) {
+                        return x(d.value1)
+                    } else {
+                        return x(d.value2);
+                    }
+                })
+                .attr("cy", d => y(d.project))
+                .attr("r", "6")
+                .style("fill", "#4C4082");
+
+        // Add the brushing
+        plot.append("g")
+            .attr("class", "brush")
+            .call(brush);
+
+        // A function that set idleTimeOut to null
+        let idleTimeout
+        function idled() { idleTimeout = null; }
+
+        // A function that update the chart for given boundaries
+        function updateChart(event) {
+            
+            let extent = event.selection
+
+            // If no selection, back to initial coordinate. Otherwise, update X axis domain
+            if (!extent) {
+                if (!idleTimeout) return idleTimeout = setTimeout(idled, 350); // This allows to wait a little bit
+                x.domain(d3.extent(metrics, d => d.timestamp));
+            } else {
+                x.domain([ x.invert(extent[0]), x.invert(extent[1]) ])
+                plot.select(".brush").call(brush.move, null) // This remove the grey brush area as soon as the selection has been done
+            }
+
+            // Update axis and circle position
+            xAxis.transition().duration(1000).call(d3.axisBottom(x).tickFormat(dayTimeFormat))
+            // Rotate axis labels
+            xAxis.selectAll("text")
+                .style("text-anchor", "end")
+                .attr("dx", "-.8em")
+                .attr("dy", ".15em")
+                .attr("transform", "rotate(-65)");
+
+            plot.selectAll(".line")
+                .transition()
+                .duration(1000)
+                .attr("x1", d => x(d.value1))
+                .attr("x2", d => {
+                    if (d.running) {
+                        return x(d.value1)
+                    } else {
+                        return x(d.value2)
+                    }
+                })
+                .attr("y1", d => y(d.project))
+                .attr("y2", d => y(d.project))
+                .attr("stroke", d => {
+                    if (d.failed) {
+                        return "red";
+                    } else if (d.success) {
+                        return "green";
+                    } else {
+                        return "blue";
+                    }
+                });
+
+            plot.selectAll(".circle1")
+                .transition().duration(1000)
+                .attr("cx", d => x(d.value1))
+                .attr("cy", d => y(d.project))
+
+            plot.selectAll(".circle2")
+                .attr("cx", d => {
+                    if (d.running) {
+                        return x(d.value1)
+                    } else {
+                        return x(d.value2);
+                    }
+                })
+                .attr("cy", d => y(d.project))
+        }
     }
 
     static updatePipelineProgressTable(data) {
