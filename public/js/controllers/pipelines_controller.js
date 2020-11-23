0e91b581b58ebd3b97147b8430800765c89cd1a5
Daniel Mwendwa <danielmwendwam@gmail.com>
2020-11-23 16:14:23 +0300

    func that transforms json to array

--- public/js/controllers/pipelines_controller.js
+++ public/js/controllers/pipelines_controller.js
@@ -15,4 +15,14 @@ export class PipelinesController {
     static jsonKeyValueToArray(k, v) {
         return [k, v];
     }
+
+    static jsonToArray(json) {
+        let arr = [];
+        for (const key in json) {
+            if (json.hasOwnProperty(key)) {
+                arr.push(PipelinesController.jsonKeyValueToArray(key, json[key]));
+            }
+        }
+        return arr;
+    }
 }
