1e734019139ac7c4349291ded698501e36118700
elvinmworia <elvin.mworia@africasvoices.org>
2023-04-01 15:47:37 +0300

    Made the requested naming changes

--- public/js/controllers/ui_controller.js
+++ public/js/controllers/ui_controller.js
@@ -336,11 +336,11 @@ export class UIController {
         UIController.statusBody.insertAdjacentHTML("beforeend", html);
     }
 
-  static noActivePipelineAlertMsg(msg) {
+  static showNoDataAlert(msg) {
         UIController.resetUI();
         let html = `<div class="container">
             <div class="card border border-0">
-            <span class=' p-2  mx-auto ' id='alertNopipeline'>${msg}</span>
+            <span class=' p-2  mx-auto ' id='alertNoData'>${msg}</span>
             </div>
         </div> `;
         UIController.statusBody.insertAdjacentHTML("beforeend", html);
